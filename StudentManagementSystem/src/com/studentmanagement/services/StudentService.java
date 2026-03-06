f3b9e875eab6a3cc4a897c40d3ea4dcf2043eb91
ngtaiphat1501 <taiciute105@gmail.com>
2026-03-06 17:55:23 +0700

    Ngtai update models StudentService

--- StudentManagementSystem/src/com/studentmanagement/services/StudentService.java
+++ StudentManagementSystem/src/com/studentmanagement/services/StudentService.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.studentmanagement.services;
 
-/**
- *
- * @author TUF GAMING
- */
-public class StudentService {
+import com.studentmanagement.models.Student;
+import java.util.List;
+import java.util.Scanner;
+
+public interface StudentService {
+    // 1.1. Thêm sinh viên
+    void addStudent(Scanner scanner);
     
+    // 1.2. Sửa thông tin sinh viên
+    void updateStudent(String studentId, Scanner scanner);
+    
+    // 1.3. Xóa sinh viên
+    boolean deleteStudent(String studentId);
+    
+    // 1.4. Tìm kiếm sinh viên
+    List<Student> searchStudent(String searchType, String keyword);
+    
+    // 1.5. Xem tất cả sinh viên
+    List<Student> getAllStudents();
+    
+    // Xuất ra Excel
+    void exportToExcel(String filePath);
 }
