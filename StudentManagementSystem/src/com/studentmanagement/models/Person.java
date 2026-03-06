5bb7fc3a8c7a5be411b29834d8f40f54768b40fe
ngtaiphat1501 <taiciute105@gmail.com>
2026-03-06 17:17:08 +0700

    Ngtai update models Person

--- StudentManagementSystem/src/com/studentmanagement/models/Person.java
+++ StudentManagementSystem/src/com/studentmanagement/models/Person.java
@@ -1,14 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.studentmanagement.models;
 
-/**
- *
- * @author TUF GAMING
- */
-public class Person {
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public abstract class Person implements Serializable {
+    protected String id;
+    protected String fullName;
+    protected Date birthDate;
+    protected String gender;
+    protected String phone;
+    protected String email;
+    protected String address;
     
+    public Person(String id, String fullName, Date birthDate, String gender, 
+                  String phone, String email, String address) {
+        this.id = id;
+        this.fullName = fullName;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+    
+    public abstract void displayInfo();
+    
+    // Getter và Setter
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+    
+    public Date getBirthDate() { return birthDate; }
+    public void setBirthDate(Date birthDate) { this.birthDate = birthDate; }
+    
+    public String getGender() { return gender; }
+    public void setGender(String gender) { this.gender = gender; }
+    
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+    
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+    
+    public String getFormattedBirthDate() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        return sdf.format(birthDate);
+    }
 }
