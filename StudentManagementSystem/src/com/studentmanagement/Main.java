b61a5c14aa558d185560c2634e6e5006c3783ac5
ngtaiphat1501 <taiciute105@gmail.com>
2026-03-09 15:28:29 +0700

    ngtai update Main

--- StudentManagementSystem/src/com/studentmanagement/Main.java
+++ StudentManagementSystem/src/com/studentmanagement/Main.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.studentmanagement;
 
-/**
- *
- * @author TUF GAMING
- */
+import com.studentmanagement.managers.UserManager;
+import com.studentmanagement.managers.StudentManager;
+import com.studentmanagement.menus.MainMenu;
+import com.studentmanagement.utils.ConsoleUtils;
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        UserManager userManager = new UserManager();
+        StudentManager studentManager = new StudentManager();
+        
+        // Tải dữ liệu từ file
+        ConsoleUtils.showLoading("Đang tải dữ liệu hệ thống...");
+        
+        // Tạo tài khoản mặc định
+        userManager.createDefaultAccounts();
+        
+        // Hiển thị menu chính
+        MainMenu mainMenu = new MainMenu(scanner, userManager, studentManager);
+        mainMenu.show();
+        
+        scanner.close();
+        System.out.println("\n👋 Cảm ơn đã sử dụng hệ thống!");
+    }
 }
